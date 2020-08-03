f7b966f9c6c0eefc228f8ec504657fa9824c203e
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2020-08-03 16:17:11 -0500

    Reorganized security properties management

--- src/main/java/com/kentcarmine/multitopicforum/config/SecurityConfig.java
+++ src/main/java/com/kentcarmine/multitopicforum/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -23,7 +24,7 @@ import javax.sql.DataSource;
  * Handles security and security-related configuration.
  */
 @Configuration
-@PropertySource("classpath:security.properties")
+@PropertySources({@PropertySource(value = "classpath:security-prod.properties"), @PropertySource(value = "classpath:security.properties", ignoreResourceNotFound = true)})
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
