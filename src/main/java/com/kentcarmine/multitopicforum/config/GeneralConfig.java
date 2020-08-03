7f0b019604196370c1e727dd1db03ca3a30bfceb
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2020-08-03 15:57:00 -0500

    Moved system.properties to root

--- src/main/java/com/kentcarmine/multitopicforum/config/GeneralConfig.java
+++ src/main/java/com/kentcarmine/multitopicforum/config/GeneralConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
